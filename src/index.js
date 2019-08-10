96d913d12ece4cba8f01b56c3922d32571459e1f
Bradley Koskowich <bkoskowich@islander.tamucc.edu>
2019-08-10 11:27:19 -0500

    Added scoping options for returning ephemeral single tiles or entire branches down to tiles. An enhancment could be to have them only delete tiles beyond the indexMaxZoom, instead of the entire tileset?

--- src/index.js
+++ src/index.js
@@ -16,10 +16,7 @@ function GeoJSONVT(data, options) {
     const debug = options.debug;
 
     const useStream = options.useStream;
-    if (useStream){
-        this.rs = new Readable({ objectMode: options.streamObject ,highWaterMark:1});
-        this.rs._read = () => {};
-    }
+
 
     if (debug) console.time('preprocess data');
 
@@ -65,10 +62,10 @@ GeoJSONVT.prototype.options = {
     debug: 0, // logging level (0, 1 or 2)
     useStream: false, // option for emitting tiles to a stream as they are generated. Will not be usable as a normal tileIndex if true, as stream is self-cleaning to keep memory down as much as possible.
     streamObject: true, // if streaming, the default mode to stream with is in object mode, instead of string/buffer mode
-    debugStream:false //display streaming debug logs
+    debugStream: false //display streaming debug logs
 };
 
-GeoJSONVT.prototype.splitTile = function(features, z, x, y, cz, cx, cy, computeonly = false) {
+GeoJSONVT.prototype.splitTile = function(features, z, x, y, cz, cx, cy, persist = true) {
 
     const stack = [features, z, x, y];
     const options = this.options;
@@ -80,137 +77,244 @@ GeoJSONVT.prototype.splitTile = function(features, z, x, y, cz, cx, cy, computeo
     // console.log(options)
 
 
-    if (debug > 1 && useStream === true) console.log("writing to stream")
+    if (debugStream) console.log("writing to stream")
 
-    // avoid recursion by using a processing queue
     var lastZ = null;
-    while (stack.length) {
-        y = stack.pop();
-        x = stack.pop();
-        z = stack.pop();
-        features = stack.pop();
+    if (useStream) {
+        var rs = new Readable({
+            objectMode: true,
 
-        // console.log(y,x,z)
+        });
+        this.rs = rs;
+        rs.stack = stack;
+        rs.lastZ = lastZ;
+        rs.tiles = this.tiles;
+        rs.tileCoords = this.tileCoords;
+        rs._read = function(size){
+            while (this.stack.length) {
+                y = this.stack.pop();
+                x = this.stack.pop();
+                z = this.stack.pop();
+                features = this.stack.pop();
 
-        const z2 = 1 << z;
-        const id = toID(z, x, y);
-        let tile = this.tiles[id];
+                // console.log(y,x,z)
 
-        if (!tile) {
-            // if (debug > 1) console.time('creation');
+                const z2 = 1 << z;
+                const id = toID(z, x, y);
+                let tile = this.tiles[id];
 
-            tile = this.tiles[id] = createTile(features, z, x, y, options);
-            this.tileCoords.push({ z, x, y });
-            // console.log(`lastZ:${lastZ}, tile.z:${tile.z}`)
-            if(debug > 1 && computeonly){console.log("tile is computeonly")}
-            if (useStream) {
-                // if(useStream) {}
-                this.rs.push(transform(this.tiles[id], options.extent));
-                if (lastZ === null) {
-                    lastZ = tile.z
-                    this.rs.resume();
-                }
-                if (tile.z === lastZ + 2) { //once an n+2 layer is reached, start deleting the parent tiles above it as they will have been passed already.
-                    if (debugStream) console.log("finding keys to omit")
-                    var omitKeys = _.filter(this.tileCoords, (key) => { return key.z === lastZ });
-                    // if(debug > 1)console.timeEnd("finding keys to omit")
-                    if (debugStream) console.log(`will omit ${JSON.stringify(omitKeys)} b/c on zoom level ${tile.z}`)
-                    if (debugStream) console.log("generating ids to omit")
-                    var omitIds = _.map(omitKeys, (key) => { return toID(key.z, key.x, key.y) });
-                    // if(debug > 1)console.timeEnd("generating ids to omit")
-                    if (debugStream) console.log("omitting keys")
-                    this.tileCoords = _.reject(this.tileCoords, (akey) => {
-                        return _.some(omitKeys, (bkey) => {
-                            return akey.z === bkey.z && akey.x === bkey.x && akey.y === bkey.y
+                if (!tile) {
+                    // if (debug > 1) console.time('creation');
+
+                    tile = this.tiles[id] = createTile(features, z, x, y, options);
+                    this.tileCoords.push({ z, x, y });
+                    // console.log(`lastZ:${lastZ}, tile.z:${tile.z}`)
+                    // if (debug > 1 && computeonly) { console.log("tile is computeonly") }
+                    // if (useStream) {
+                    // if(useStream) {}
+                    rs.push(transform(this.tiles[id], options.extent));
+                    if (lastZ === null) {
+                        lastZ = tile.z
+                    }
+                    if (tile.z === lastZ + 2) { //once an n+2 layer is reached, start deleting the parent tiles above it as they will have been passed already.
+                        if (debugStream) console.log("finding keys to omit")
+                        var omitKeys = _.filter(this.tileCoords, (key) => { return key.z === lastZ });
+                        // if(debug > 1)console.timeEnd("finding keys to omit")
+                        if (debugStream) console.log(`will omit ${JSON.stringify(omitKeys)} b/c on zoom level ${tile.z}`)
+                        if (debugStream) console.log("generating ids to omit")
+                        var omitIds = _.map(omitKeys, (key) => { return toID(key.z, key.x, key.y) });
+                        // if(debug > 1)console.timeEnd("generating ids to omit")
+                        if (debugStream) console.log("omitting keys")
+                        this.tileCoords = _.reject(this.tileCoords, (akey) => {
+                            return _.some(omitKeys, (bkey) => {
+                                return akey.z === bkey.z && akey.x === bkey.x && akey.y === bkey.y
+                            });
                         });
-                    });
-                    // if(debug > 1)console.timeEnd("omitting keys")
-                    if (debugStream) console.log("omitting tiles")
-                    this.tiles = _.omit(this.tiles, omitIds);
-                    // if(debug > 1)console.timeEnd("omitting tiles")
-                    if (debugStream) console.log(`now have ${_.keys(this.tiles).length} tiles cached`)
-                    lastZ += 1; //increment to the next 
+                        // if(debug > 1)console.timeEnd("omitting keys")
+                        if (debugStream) console.log("omitting tiles")
+                        this.tiles = _.omit(this.tiles, omitIds);
+                        // if(debug > 1)console.timeEnd("omitting tiles")
+                        if (debugStream) console.log(`now have ${_.keys(this.tiles).length} tiles cached`)
+                        lastZ += 1; //increment to the next 
+                    }
+
+                    // }
+
+
+                    if (debug) {
+                        if (debug > 1) {
+                            console.log('tile z%d-%d-%d (features: %d, points: %d, simplified: %d)',
+                                z, x, y, tile.numFeatures, tile.numPoints, tile.numSimplified);
+                            // console.timeEnd('creation');
+                        }
+                        const key = `z${  z}`;
+                        this.stats[key] = (this.stats[key] || 0) + 1;
+                        this.total++;
+                    }
                 }
 
-            }
+                // save reference to original geometry in tile so that we can drill down later if we stop now
+                tile.source = features;
 
+                // if it's the first-pass tiling
+                if (!cz) {
+                    // stop tiling if we reached max zoom, or if the tile is too simple
+                    if (z === options.indexMaxZoom || tile.numPoints <= options.indexMaxPoints) continue;
 
-            if (debug) {
-                if (debug > 1) {
-                    console.log('tile z%d-%d-%d (features: %d, points: %d, simplified: %d)',
-                        z, x, y, tile.numFeatures, tile.numPoints, tile.numSimplified);
-                    // console.timeEnd('creation');
+                    // if a drilldown to a specific tile
+                } else {
+                    // stop tiling if we reached base zoom or our target tile zoom
+                    if (z === options.maxZoom || z === cz) continue;
+
+                    // stop tiling if it's not an ancestor of the target tile
+                    const m = 1 << (cz - z);
+                    if (x !== Math.floor(cx / m) || y !== Math.floor(cy / m)) continue;
                 }
-                const key = `z${  z}`;
-                this.stats[key] = (this.stats[key] || 0) + 1;
-                this.total++;
+
+                // if we slice further down, no need to keep source geometry
+                tile.source = null;
+
+                if (features.length === 0) continue;
+
+                if (debug > 1) console.time('clipping');
+
+                // values we'll use for clipping
+                const k1 = 0.5 * options.buffer / options.extent;
+                const k2 = 0.5 - k1;
+                const k3 = 0.5 + k1;
+                const k4 = 1 + k1;
+
+                let tl = null;
+                let bl = null;
+                let tr = null;
+                let br = null;
+
+                let left = clip(features, z2, x - k1, x + k3, 0, tile.minX, tile.maxX, options);
+                let right = clip(features, z2, x + k2, x + k4, 0, tile.minX, tile.maxX, options);
+                features = null;
+
+                if (left) {
+                    tl = clip(left, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
+                    bl = clip(left, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
+                    left = null;
+                }
+
+                if (right) {
+                    tr = clip(right, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
+                    br = clip(right, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
+                    right = null;
+                }
+
+                if (debug > 1) console.timeEnd('clipping');
+
+                this.stack.push(tl || [], z + 1, x * 2, y * 2);
+                this.stack.push(bl || [], z + 1, x * 2, y * 2 + 1);
+                this.stack.push(tr || [], z + 1, x * 2 + 1, y * 2);
+                this.stack.push(br || [], z + 1, x * 2 + 1, y * 2 + 1);
             }
+            if (debugStream && useStream) console.log("stream generation end")
+            if (useStream) rs.push(null);
         }
+        
+    } else {
+        // avoid recursion by using a processing queue
 
-        // save reference to original geometry in tile so that we can drill down later if we stop now
-        tile.source = features;
+        while (stack.length) {
+            y = stack.pop();
+            x = stack.pop();
+            z = stack.pop();
+            features = stack.pop();
 
-        // if it's the first-pass tiling
-        if (!cz) {
-            // stop tiling if we reached max zoom, or if the tile is too simple
-            if (z === options.indexMaxZoom || tile.numPoints <= options.indexMaxPoints) continue;
+            // console.log(y,x,z)
 
-            // if a drilldown to a specific tile
-        } else {
-            // stop tiling if we reached base zoom or our target tile zoom
-            if (z === options.maxZoom || z === cz) continue;
+            const z2 = 1 << z;
+            const id = toID(z, x, y);
+            let tile = this.tiles[id];
 
-            // stop tiling if it's not an ancestor of the target tile
-            const m = 1 << (cz - z);
-            if (x !== Math.floor(cx / m) || y !== Math.floor(cy / m)) continue;
+            if (!tile) {
+                // if (debug > 1) console.time('creation');
+
+                tile = this.tiles[id] = createTile(features, z, x, y, options);
+                this.tileCoords.push({ z, x, y });
+                if (debug) {
+                    if (debug > 1) {
+                        console.log('tile z%d-%d-%d (features: %d, points: %d, simplified: %d)',
+                            z, x, y, tile.numFeatures, tile.numPoints, tile.numSimplified);
+                        // console.timeEnd('creation');
+                    }
+                    const key = `z${  z}`;
+                    this.stats[key] = (this.stats[key] || 0) + 1;
+                    this.total++;
+                }
+            }
+
+            // save reference to original geometry in tile so that we can drill down later if we stop now
+            tile.source = features;
+
+            // if it's the first-pass tiling
+            if (!cz) {
+                // stop tiling if we reached max zoom, or if the tile is too simple
+                if (z === options.indexMaxZoom || tile.numPoints <= options.indexMaxPoints) continue;
+
+                // if a drilldown to a specific tile
+            } else {
+                // stop tiling if we reached base zoom or our target tile zoom
+                if (z === options.maxZoom || z === cz) continue;
+
+                // stop tiling if it's not an ancestor of the target tile
+                const m = 1 << (cz - z);
+                if (x !== Math.floor(cx / m) || y !== Math.floor(cy / m)) continue;
+            }
+
+            // if we slice further down, no need to keep source geometry
+            tile.source = null;
+
+            if (features.length === 0) continue;
+
+            if (debug > 1) console.time('clipping');
+
+            // values we'll use for clipping
+            const k1 = 0.5 * options.buffer / options.extent;
+            const k2 = 0.5 - k1;
+            const k3 = 0.5 + k1;
+            const k4 = 1 + k1;
+
+            let tl = null;
+            let bl = null;
+            let tr = null;
+            let br = null;
+
+            let left = clip(features, z2, x - k1, x + k3, 0, tile.minX, tile.maxX, options);
+            let right = clip(features, z2, x + k2, x + k4, 0, tile.minX, tile.maxX, options);
+            features = null;
+
+            if (left) {
+                tl = clip(left, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
+                bl = clip(left, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
+                left = null;
+            }
+
+            if (right) {
+                tr = clip(right, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
+                br = clip(right, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
+                right = null;
+            }
+
+            if (debug > 1) console.timeEnd('clipping');
+
+            stack.push(tl || [], z + 1, x * 2, y * 2);
+            stack.push(bl || [], z + 1, x * 2, y * 2 + 1);
+            stack.push(tr || [], z + 1, x * 2 + 1, y * 2);
+            stack.push(br || [], z + 1, x * 2 + 1, y * 2 + 1);
         }
-
-        // if we slice further down, no need to keep source geometry
-        tile.source = null;
-
-        if (features.length === 0) continue;
-
-        if (debug > 1) console.time('clipping');
-
-        // values we'll use for clipping
-        const k1 = 0.5 * options.buffer / options.extent;
-        const k2 = 0.5 - k1;
-        const k3 = 0.5 + k1;
-        const k4 = 1 + k1;
-
-        let tl = null;
-        let bl = null;
-        let tr = null;
-        let br = null;
-
-        let left = clip(features, z2, x - k1, x + k3, 0, tile.minX, tile.maxX, options);
-        let right = clip(features, z2, x + k2, x + k4, 0, tile.minX, tile.maxX, options);
-        features = null;
-
-        if (left) {
-            tl = clip(left, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
-            bl = clip(left, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
-            left = null;
-        }
-
-        if (right) {
-            tr = clip(right, z2, y - k1, y + k3, 1, tile.minY, tile.maxY, options);
-            br = clip(right, z2, y + k2, y + k4, 1, tile.minY, tile.maxY, options);
-            right = null;
-        }
-
-        if (debug > 1) console.timeEnd('clipping');
-
-        stack.push(tl || [], z + 1, x * 2, y * 2);
-        stack.push(bl || [], z + 1, x * 2, y * 2 + 1);
-        stack.push(tr || [], z + 1, x * 2 + 1, y * 2);
-        stack.push(br || [], z + 1, x * 2 + 1, y * 2 + 1);
+        console.log("normal generation end")
     }
-    if (debugStream && useStream) console.log("stream input end")
-    if (useStream) this.rs.push(null);
+
+
+
 };
 
-GeoJSONVT.prototype.getTile = function(z, x, y, computeonly = false) {
+GeoJSONVT.prototype.getTile = function(z, x, y, persist = true, returnbranch = false) {
     z = +z;
     x = +x;
     y = +y;
@@ -246,10 +350,41 @@ GeoJSONVT.prototype.getTile = function(z, x, y, computeonly = false) {
     if (debug > 1) console.log('found parent tile z%d-%d-%d', z0, x0, y0);
 
     if (debug > 1) console.time('drilling down');
-    this.splitTile(parent.source, z0, x0, y0, z, x, y, computeonly);
+    this.splitTile(parent.source, z0, x0, y0, z, x, y, persist);
     if (debug > 1) console.timeEnd('drilling down');
 
-    return this.tiles[id] ? transform(this.tiles[id], extent) : null;
+    if(persist === true){
+        return this.tiles[id] ? transform(this.tiles[id], extent) : null;
+    }
+    else{
+        //find the index in the tile coords which has the key
+        var breakIdx = _.findIndex(this.tileCoords, (tilekey) =>{
+            return tilekey.z === z && tilekey.x === x && tilekey.y === y
+        });
+
+        // console.log(breakIdx)
+        // console.log(this.tileCoords)
+        var spike = this.tileCoords.slice(0,breakIdx+1)
+        var sorted = _.sortBy(spike, (obj) => {
+            return obj.z;
+        }).reverse();
+        // console.log(sorted)
+        var tileArray = _.map(sorted, (key) => {return this.tiles[toID(key.z, key.x, key.y)]})
+        if(debug > 1) console.log("cleaning tiles")
+        this.tiles = {};
+        if(debug > 1) console.log("cleaning tileCoords")
+        this.tileCoords = [];
+
+        if(returnbranch){
+            if(debug > 1) console.log("returning tile with branch")
+            return tileArray
+        }
+        else{
+            if(debug > 1) console.log("returning tile")
+            return tileArray[0]   
+        }
+        
+    }
 };
 
 function toID(z, x, y) {
